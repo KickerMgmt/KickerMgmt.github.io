f22a54aceab24b9d6ed2fa5bb936dfd93c4c0fd6
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2021-03-19 15:48:39 +0100

    updates

---
+++ _build/createTeams.ps1
@@ -0,0 +1,8 @@
+﻿$basisPfad = "$PSScriptRoot\.."
+
+foreach ($teamFolder in Get-ChildItem "$basisPfad\_database\teams") {
+    $teamName = $teamFolder.Name
+    Write-Host "Verarbeite Team: $teamName"
+
+    $teamName | Set-Content -Path "$basisPfad\Teams\$teamName.md"
+}
